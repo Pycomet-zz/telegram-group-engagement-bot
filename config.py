55e543da1d65b3265ac1d0fac2aeb9fed50e6b7b
Codefred <alfredemmanuelinyang@gmail.com>
2020-07-02 15:11:52 +0100

    new update

--- config.py
+++ config.py
@@ -8,12 +8,13 @@ from flask import Flask, request
 # Telegram variables
 TOKEN = os.getenv("TOKEN")
 
-
-
 # Instagram variables
 USERNAME = os.getenv("USERNAME")
 PASSWORD = os.getenv("PASSWORD")
 
+WEBHOOK_URL = os.getenv("WEBHOOK_URL")
+
+bot = telebot.AsyncTeleBot(TOKEN, threaded=True)
 
 import importdir
 importdir.do("main/handlers", globals())
